9fa78fbe776819f2a7b9fea9a001e27fc87ba44f
phongphat <yotsivapui@gmail.com>
2026-04-06 11:43:35 +0700

    learnoop1

---
+++ ัtest.py
@@ -0,0 +1,10 @@
+class Tank:
+      def __init__(self, name, ammo):
+            self.name = name
+            self.ammo = ammo
+Tank1 = Tank("Panzer", 100)
+print(Tank1.name)            
+print(Tank1.ammo)
+Tank2 = Tank("Tiger", 150)
+print(Tank2.name)
+print(Tank2.ammo)
