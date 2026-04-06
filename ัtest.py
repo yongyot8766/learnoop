94a64f7b9391e2b6f983c92c5ca027b90aa60465
phongphat <yotsivapui@gmail.com>
2026-04-06 11:56:24 +0700

    d2

--- ัtest.py
+++ ัtest.py
@@ -1,10 +1,14 @@
-class Tank:
-      def __init__(self, name, ammo):
-            self.name = name
-            self.ammo = ammo
-Tank1 = Tank("Panzer", 100)
-print(Tank1.name)            
-print(Tank1.ammo)
-Tank2 = Tank("Tiger", 150)
-print(Tank2.name)
-print(Tank2.ammo)
+class Book:
+      def __init__(self, title, author, pages):
+            self.title = title
+            self.author = author
+            self.pages = pages
+
+Book1 = Book("The Great Gatsby", "F. Scott Fitzgerald", 180)
+Book2 = Book("To Kill a Mockingbird", "Harper Lee", 281)     
+print(Book1.title)
+print(Book2.title)
+print(Book1.author)
+print(Book2.author)
+print(Book1.pages)
+print(Book2.pages)
