0b07d821a622dbc94b739e5cfc6e66147761890a
phongphat <yotsivapui@gmail.com>
2026-04-08 10:33:16 +0700

    yongyot

--- ัtest.py
+++ ัtest.py
@@ -1,18 +1,7 @@
-#create class
 class Employee:
-    def __init__(self, name, age, salary):
-        self.name = name
-        self.age = age
-        self.salary = salary
-
-    def display(self):
-        print(f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}") 
-# create object
-emp1 = Employee("John Doe", 30, 50000)
-emp2 = Employee("Jane Smith", 25, 60000)
-# call method
-emp1.display()
-emp2.display()  
-emp3 = Employee("Alice Johnson", 28, 55000)
-emp3.display()
-    
+      def __init__(self,name , age):
+            
+        self.name=name
+        self.age=age                  
+emp1=Employee("yong",66)
+print(f"name: {emp1.name}  age: {emp1.age}")
