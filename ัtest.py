0148d7c6bc01355c21b186e09bc582d6a3ecd13b
phongphat <yotsivapui@gmail.com>
2026-04-12 11:39:47 +0700

    pui12

--- ัtest.py
+++ ัtest.py
@@ -23,7 +23,7 @@ people1.introduce()    '''
 human1=Human('John',22)            
 human1.aging(3)'''
 
-class Human:
+'''class Human:
       def __init__(self,name,age):
             self.name=name
             self.age=age
@@ -33,4 +33,23 @@ class Human:
             print(f'After {human1.age}years old')
 
 human1=Human('John',23)
-human1.aging(3)            
+human1.aging(3)'''
+a=3
+b=2
+
+'''print(f'result= {a+b} bath')   
+print(f'a+b= {a+b}')         
+print('%d'%a)
+print('%f '%a)
+print('%.2f'%a)'''
+
+firstname='yongyot'
+lastname='thanaaseneewat'
+
+print('my name is ',firstname+'.')
+
+number=int(input('Enter your name: '))
+if number%2==0:
+      print('เลขคู่ เลขคี่ เก')
+else:
+      print('เลขคี่')      
